f8c8723af7ff6a74909c2c737f38be9427357cfc
Sneha <snhmakwana94@gmail.com>
2026-06-30 15:06:32 -0400

    Initial Commit

---
+++ src/main/java/com/sneha/compensation/App.java
@@ -0,0 +1,13 @@
+package com.sneha.compensation;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
