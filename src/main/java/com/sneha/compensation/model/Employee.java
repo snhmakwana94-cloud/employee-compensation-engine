0a3ea432a5eab2de98cb054fcb3fb6a56cc76a0c
Sneha <snhmakwana94@gmail.com>
2026-06-30 15:20:28 -0400

    configured maven for Java 21

---
+++ src/main/java/com/sneha/compensation/model/Employee.java
@@ -0,0 +1,9 @@
+package com.sneha.compensation.model;
+
+public class Employee {
+    private int employeeId;
+    private String name;
+    private String department;
+    private double baseSalary;
+    private int experienceYears;
+}
